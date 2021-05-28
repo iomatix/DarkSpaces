a7c9b727093d05ebff6357c1e71328808182f6b2
iomatix <mateuszwypchlak@gmail.com>
2021-05-28 19:43:22 +0200

    Initial Resources

---
+++ Assets/Resources/Particles/Visuals/Effects/EffectHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectHandler : MonoBehaviour
+{
+
+    public float LifeSpan = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, LifeSpan);
+    }
+
+}
